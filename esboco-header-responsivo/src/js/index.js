9b69058611811ae7007cccdac8923f4dddde9e36
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-04-27 23:05:01 -0300

    header desktop finished

--- esboco-header-responsivo/src/js/index.js
+++ esboco-header-responsivo/src/js/index.js
@@ -1,17 +1,85 @@
+// desktop
+
+const bottomHeader = document.querySelector('.bottom-header');
+
+const menuExpand = document.querySelector('.menu-expand > p, .menu-expand > span');
+const spanCategorias = document.querySelector('.span-categorias');
+const subMenu = document.querySelector('.sub-menu');
+
+const subMenuExpand1 = document.querySelectorAll('.sub-menu-expand > p')[0];
+const spanPecas = document.querySelector('.span-pecas');
+const subMenuPecas = document.querySelectorAll('.sub-sub-menu')[0];
+
+const subMenuExpand2 = document.querySelectorAll('.sub-menu-expand > p')[1];
+const spanAcessorios = document.querySelector('.span-acessorios');
+const subMenuAcessorios = document.querySelectorAll('.sub-sub-menu')[1];
+
+menuExpand.onclick = () => {
+    if (spanCategorias.innerHTML === '❯') {
+        spanCategorias.innerHTML = '❮';
+        subMenu.style.display = 'flex';
+    } else {
+        spanCategorias.innerHTML = '❯';
+        subMenu.style.display = 'none';
+    }
+    if (subMenuPecas.style.display !== 'none' || subMenuAcessorios.style.display !== 'none') {
+        
+        spanPecas.innerHTML = '❯';
+        subMenuPecas.style.display = 'none';
+
+        spanAcessorios.innerHTML = '❯';
+        subMenuAcessorios.style.display = 'none';
+    }
+}
+
+subMenuExpand1.onclick = () => {
+    if (spanPecas.innerHTML === '❯') {
+        spanPecas.innerHTML = '❮';
+        subMenuPecas.style.display = 'flex';
+    } else {
+        spanPecas.innerHTML = '❯';
+        subMenuPecas.style.display = 'none';
+    }
+    if (subMenuPecas.style.display !== 'none') {
+        spanAcessorios.innerHTML = '❯';
+        subMenuAcessorios.style.display = 'none';
+    }
+}
+
+subMenuExpand2.onclick = () => {
+    if (spanAcessorios.innerHTML === '❯') {
+        spanAcessorios.innerHTML = '❮';
+        subMenuAcessorios.style.display = 'flex';
+    } else {
+        spanAcessorios.innerHTML = '❯';
+        subMenuAcessorios.style.display = 'none';
+    }
+    if (subMenuAcessorios.style.display !== 'none') {
+        spanPecas.innerHTML = '❯';
+        subMenuPecas.style.display = 'none';
+    }
+}
+
+bottomHeader.onmouseleave = () => {
+    spanCategorias.innerHTML = '❯';
+    subMenu.style.display = 'none';
+    spanAcessorios.innerHTML = '❯';
+    subMenuAcessorios.style.display = 'none';
+    spanPecas.innerHTML = '❯';
+    subMenuPecas.style.display = 'none';
+}
+
+// mobile
+
 const hamburguerMenu = document.querySelector('.header-hamburguer');
 const headerNavigation = document.querySelector('.header-navigation');
-const mediaQuery = window
 
 hamburguerMenu.onclick = () => {
     if (hamburguerMenu.innerHTML === '☰') { 
         hamburguerMenu.innerHTML = '✕';
-        headerNavigation.style.display = 'block';
+        headerNavigation.style.display = 'flex';
     } else { 
        hamburguerMenu.innerHTML = '☰';
        headerNavigation.style.display = 'none';
     }
-}
-
-mediaQuery.matchMedia('(max-width: 800px)') = () => {
-    alert('Alterado.')
-}
+};
