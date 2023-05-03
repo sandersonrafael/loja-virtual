156a7580f7001cfd2699b7c5cc3e2d9a440a0d16
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-05-03 11:58:04 -0300

    product model added

--- esboco-header-responsivo/src/js/front-page/index.js
+++ esboco-header-responsivo/src/js/front-page/index.js
@@ -73,6 +73,81 @@ bottomHeader.onmouseleave = () => {
         subMenuPecas.style.display = 'none';
 };
 
+// banner desktop
+
+const banners = document.querySelector('.banners');
+const banner1 = document.querySelector('.banner-option-1');
+const banner2 = document.querySelector('.banner-option-2');
+const banner3 = document.querySelector('.banner-option-3');
+const bannerOption1 = document.querySelectorAll('.banner-option-button')[0];
+const bannerOption2 = document.querySelectorAll('.banner-option-button')[1];
+const bannerOption3 = document.querySelectorAll('.banner-option-button')[2];
+bannerOption1.style['background-color'] = '#000000'
+
+window.onload = () => {
+    setInterval(() => {
+        if (banner1.style.display === 'block') {
+            banner1.style.display = 'none';
+            bannerOption1.style['background-color'] = 'transparent';
+
+            banner2.style.display = 'block';
+            bannerOption2.style['background-color'] = '#000000';
+
+            banner3.style.display = 'none';
+            bannerOption3.style['background-color'] = 'transparent';
+        } else if (banner2.style.display === 'block') {
+            banner1.style.display = 'none';
+            bannerOption1.style['background-color'] = 'transparent';
+
+            banner2.style.display = 'none';
+            bannerOption2.style['background-color'] = 'transparent';
+
+            banner3.style.display = 'block';
+            bannerOption3.style['background-color'] = '#000000';
+        } else {
+            banner1.style.display = 'block';
+            bannerOption1.style['background-color'] = '#000000';
+
+            banner2.style.display = 'none';
+            bannerOption2.style['background-color'] = 'transparent';
+
+            banner3.style.display = 'none';
+            bannerOption3.style['background-color'] = 'transparent';
+        }
+    }, 5000);
+};
+
+bannerOption1.onclick = () => {
+    banner1.style.display = 'block';
+    bannerOption1.style['background-color'] = '#000000';
+
+    banner2.style.display = 'none';
+    bannerOption2.style['background-color'] = 'transparent';
+
+    banner3.style.display = 'none';
+    bannerOption3.style['background-color'] = 'transparent';
+};
+bannerOption2.onclick = () => {
+    banner1.style.display = 'none';
+    bannerOption1.style['background-color'] = 'transparent';
+
+    banner2.style.display = 'block';
+    bannerOption2.style['background-color'] = '#000000';
+
+    banner3.style.display = 'none';
+    bannerOption3.style['background-color'] = 'transparent';
+};
+bannerOption3.onclick = () => {
+    banner1.style.display = 'none';
+    bannerOption1.style['background-color'] = 'transparent';
+
+    banner2.style.display = 'none';
+    bannerOption2.style['background-color'] = 'transparent';
+
+    banner3.style.display = 'block';
+    bannerOption3.style['background-color'] = '#000000';
+};
+
 // footer mobile
 
 const footerCustomerExpand = document.querySelector('.footer-customer-area-expand');
@@ -160,17 +235,3 @@ headerBack.onclick = () => {
         headerBack.style.display = 'none';
     }
 };
-
-
-// cookies e json para o site
-
-/* console.log(document.cookie) */
-/* fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => console.log(json)) */
-
-/* no json, para adicionar produtos, pode-se utilizar o método push dos arrays para adicionar produtos ao json
-pode-se utilizar o método slice para apagar determinados produtos
-necessário proteger o json para acessos externos
-
-*/
