99144c66b9aab2362523f74d62c1edee041bad9f
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-05-05 19:40:58 -0300

    att description

--- prod-model/script.js
+++ prod-model/script.js
@@ -1,6 +1,6 @@
 const aditionalImgs = document.querySelector('.product-aditional-imgs');
 
-// aditionalImgs.innerHTML = '<div class="product-aditional-img"><img class="aditional-img" src="img/products/prancha-de-skate-santa-cruz-ryb-color/main-image.jpeg" alt=""></div>';
+aditionalImgs.innerHTML = '<div class="product-aditional-img"><img class="aditional-img" src="img/products/prancha-de-skate-santa-cruz-ryb-color/main-image.jpeg" alt=""></div>';
 aditionalImgs.innerHTML += '<div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-1.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-2.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-3.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-4.jpeg" class="aditional-img"></div>';
 
 const aditionalImg = document.querySelectorAll('.product-aditional-img');
