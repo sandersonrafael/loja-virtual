6e25d84859baafc1c194376215b9a9f13b38da97
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-05-05 12:09:30 -0300

    product model buttons added

--- prod-model/script.js
+++ prod-model/script.js
@@ -1,1 +1,12 @@
-document.querySelector('.product-aditional-imgs').innerHTML = '<div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-1.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-2.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-3.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-4.jpeg" class="aditional-img"></div>'
+const aditionalImgs = document.querySelector('.product-aditional-imgs');
+
+aditionalImgs.innerHTML = '<div class="product-aditional-img"><img class="aditional-img" src="img/products/prancha-de-skate-santa-cruz-ryb-color/main-image.jpeg" alt=""></div>';
+aditionalImgs.innerHTML += '<div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-1.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-2.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-3.jpeg" class="aditional-img"></div><div class="product-aditional-img"><img src="img/products/prancha-de-skate-santa-cruz-ryb-color/aditional-image-4.jpeg" class="aditional-img"></div>';
+
+const aditionalImg = document.querySelectorAll('.product-aditional-img');
+
+if (aditionalImg.length === 1) {
+    aditionalImgs.style.display = 'none';
+} else if (aditionalImg.length > 1 && aditionalImg.length <= 4) {
+    aditionalImgs.style['justify-content'] = 'center';
+}
