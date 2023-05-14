6f1b48be3380ee062026641ade62680704cef184
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-05-13 23:39:49 -0300

    2 products added

--- src/db/products-analyzer.js
+++ src/db/products-analyzer.js
@@ -16,8 +16,9 @@ fetch('http://127.0.0.1:5500/src/db/products.json')
             'kits',
             'bags',
             'manutenção',
-            'roupas'
-        ]
+            'diversos'
+        ];
+
         console.log('\nQuantidade de produtos:', products.length)
         console.log(
             '\nProdutos em promoção:',
@@ -28,7 +29,7 @@ fetch('http://127.0.0.1:5500/src/db/products.json')
         for (let collection of collections)  {
             const quantidade = products.filter(product => product.collection === collection).length;
             console.log(collection.slice(0, 1).toUpperCase() + collection.slice(1), ':', quantidade);
-        }
+        };
 
 
         console.log('\nTodas Urls:\n');
@@ -40,7 +41,7 @@ fetch('http://127.0.0.1:5500/src/db/products.json')
         console.log('\nTodas Urls Rolamentos:\n');
         for (let product of products) if (product.collection === 'rolamentos') console.log(product.url);
 
-        console.log('\nTodas marcas de rodas:\n')
+        console.log('\nTodas marcas de rodas:\n');
         for (let product of products) if (product.collection === 'rodas') console.log(product.brand);
     })
     .catch((err) => console.log('\nFalha na conexão...\nVerifique se o servidor local está correto e conectado.'));
