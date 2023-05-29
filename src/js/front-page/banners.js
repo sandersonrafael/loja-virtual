7cd58f5f7d8400a38fe248def157ed36d8d830c8
Sanderson Rafael <sandersonrafael-35@hotmail.com>
2023-05-28 21:48:39 -0300

    banner animation and buttons added

---
+++ src/js/front-page/banners.js
@@ -0,0 +1,27 @@
+const selectedBanner = document.querySelectorAll('.banners-container input[type="radio"]');
+let slideInterval;
+const bannerSlide = () => slideInterval = setInterval(() => {
+    if (selectedBanner[0].checked === true) selectedBanner[1].checked = true;
+    else if (selectedBanner[1].checked === true) selectedBanner[2].checked = true;
+    else if (selectedBanner[2].checked === true) selectedBanner[0].checked = true;
+}, 4000);
+bannerSlide();
+
+const previousBanner = document.querySelector('.banner-previous');
+const nextBanner = document.querySelector('.banner-next');
+
+previousBanner.onclick = () => {
+    clearInterval(slideInterval);
+    if (selectedBanner[0].checked === true) selectedBanner[2].checked = true;
+    else if (selectedBanner[1].checked === true) selectedBanner[0].checked = true;
+    else if (selectedBanner[2].checked === true) selectedBanner[1].checked = true;
+    return bannerSlide();
+};
+
+nextBanner.onclick = () => {
+    clearInterval(slideInterval);
+    if (selectedBanner[0].checked === true) selectedBanner[1].checked = true;
+    else if (selectedBanner[1].checked === true) selectedBanner[2].checked = true;
+    else if (selectedBanner[2].checked === true) selectedBanner[0].checked = true;
+    return bannerSlide();
+};
